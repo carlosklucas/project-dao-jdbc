995fd6fff50f5c0f874a2d9fa09bddb8fc9e9e01
carlosklucas <carlosklucas@hotmail.com>
2026-04-29 17:41:57 +0100

    added delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 //		Department dep = new Department(1, "Books");
 //		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
@@ -45,8 +48,16 @@ public class Program {
 		seller = sellerDao.findById(1);
 		seller.setName("Carlos2");
 		sellerDao.update(seller);
-		
 		System.out.println("Updated! New id = "  + seller.getId());
+		
+		System.out.println("=== TEST 6: seller Delete ===");
+		sellerDao.delete(6);
+		System.out.println("Delete completed");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.delete(id);
+		System.out.println("Deleted id:" + id);
+		
 	}
 
 }
